7ce48d6032f54cbfcbd22fd4fa742bcb1ead50bd
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-25 18:59:51 -0300

    feat: radio component

---
+++ src/components/Radio.js
@@ -0,0 +1,23 @@
+const Radio = ({ label, choices, name, onChange, checked, error }) => {
+  return (
+    <div className="form__group form__group--radio">
+      <p>{label}</p>
+      {choices.map(choice => (
+        <label htmlFor={choice.id} key={choice.id}>
+          <input
+            type="radio"
+            name={name}
+            id={choice.id}
+            value={choice.value}
+            onChange={onChange}
+            checked={checked && checked === choice.value}
+          />
+          <span>{choice.label}</span>
+        </label>
+      ))}
+      {error && <span className="form__error">{error}</span>}
+    </div>
+  );
+};
+
+export default Radio;
