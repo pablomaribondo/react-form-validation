44405e5b76c4cf5a9e033f862e4357db36525780
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-25 19:56:53 -0300

    feat: call header & form components on main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.scss';
 
+import Header from './components/Header';
+import Form from './components/Form';
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header title="Form validation using hooks" />
+      <Form />
+    </>
+  );
 };
 
 export default App;
